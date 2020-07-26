6e2624ded22832c1604d0d9a6b9868f94beaf87c
toolCHAINZ <27028960+toolCHAINZ@users.noreply.github.com>
2020-07-26 11:30:25 -0400

    Use separate view for each format

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
-from .hex_view import HexView
+from .srec_view import SRECView
+from .ihex_view import IHexView
+from .ti_txt_view import TiTxtView
 
-HexView.register()
+SRECView.register()
+IHexView.register()
+TiTxtView.register()
---
+++ ti_txt_view.py
@@ -0,0 +1,28 @@
+from binaryninja import BinaryView, BinaryViewType
+from binaryninja.enums import SegmentFlag
+from bincopy import BinFile, is_ti_txt
+
+class TiTxtView(BinaryView):
+    name = "TI HEX"
+    @classmethod
+    def is_valid_for_data(self, data: BinaryView):
+        actual_data = data.read(0, len(data)).decode('utf8')
+        if is_ti_txt(actual_data):
+            return True
+        return False
+
+    def __init__(self, data: BinaryView):
+        actual_data = data.read(0, len(data)).decode('utf8')
+        binfile = BinFile()
+        binfile.add(actual_data)
+        decoded = b''.join(s.data for s in binfile.segments)
+        parent = BinaryView.new(data=decoded)
+        self.hex_segments = binfile.segments
+        BinaryView.__init__(self, file_metadata=data.file, parent_view=parent)
+
+    def init(self):
+        offset = 0
+        for segment in self.hex_segments:
+            length = len(segment.data)
+            self.add_auto_segment(segment.address, length, offset, length, SegmentFlag.SegmentReadable | SegmentFlag.SegmentWritable | SegmentFlag.SegmentExecutable)
+        return True
